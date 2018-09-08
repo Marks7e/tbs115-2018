5b8900822812a9d7e4faf93a0298d784ab190cf5
Marcos Barrera <mmbarrerae@gmail.com>
2018-09-08 16:32:58 -0600

    agregada navegación de menu y musica en loop

---
+++ tbs/Assets/Scripts/AutoBuild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class AutoBuild : MonoBehaviour
+{
+
+    static public string HasErrorOcurred;
+
+    static public void BuildProject()
+    {
+#if UNITY_EDITOR
+
+        HasErrorOcurred = string.Empty;
+        UnityEditor.BuildPlayerOptions buildPlayerOptions = new UnityEditor.BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "AutoBuildWin";
+        buildPlayerOptions.target = UnityEditor.BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = UnityEditor.BuildOptions.None;
+        BuildReport result = UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+        
+        Debug.Log(result.ToString());
+#endif
+    }
+}
--- tbs/Assets/Scripts/Menus/MainMenu.cs
+++ tbs/Assets/Scripts/Menus/MainMenu.cs
@@ -2,13 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    GameObject PrincipalMenu;
+    GameObject OptionsMenu;
+
+
+    void Start()
+    {
+        PrincipalMenu = GameObject.Find("PrincipalMenu");
+        OptionsMenu = GameObject.Find("OptionsMenu");
+
+        PrincipalMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game... Editor does not support QUIT event!");
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        PrincipalMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+        Debug.Log("Option button Clicked!");
+    }
+
+    public void OptionsGoBack()
+    {
+        PrincipalMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+
+        Debug.Log("Backing to PrincipalMenu...");
+    }
+
+
 }
+
