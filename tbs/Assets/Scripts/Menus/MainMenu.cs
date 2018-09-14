be496424a37416a38dd8fad80cc15961c5bc5d8f
lusart <lusart87@gmail.com>
2018-09-14 03:04:35 -0600

    Agregados botones personalizados y fondo

--- tbs/Assets/Scripts/Menus/MainMenu.cs
+++ tbs/Assets/Scripts/Menus/MainMenu.cs
@@ -6,29 +6,51 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-
     GameObject PrincipalMenu;
     GameObject OptionsMenu;
-
+	GameObject ExtrasMenu;
+	GameObject RealmsMenu;
+	GameObject StoriesMenu;
+	GameObject FamiliaMenu;
+	GameObject EscuelaMenu;
+	GameObject AutoevaluacionMenu;
+	GameObject FerrejeMenu;
+	GameObject BanMenu;
+	GameObject KanslorMenu;
 
     void Start()
     {
         PrincipalMenu = GameObject.Find("PrincipalMenu");
-        OptionsMenu = GameObject.Find("OptionsMenu");
+        OptionsMenu	= GameObject.Find("OptionsMenu");
+		ExtrasMenu 	= GameObject.Find("ExtrasMenu");
+		RealmsMenu 	= GameObject.Find("RealmsMenu");
+		StoriesMenu = GameObject.Find ("StoriesMenu");
+		FamiliaMenu = GameObject.Find ("FamiliaMenu");
+		EscuelaMenu = GameObject.Find ("EscuelaMenu");
+		AutoevaluacionMenu = GameObject.Find ("AutoevaluacionMenu");
+		FerrejeMenu = GameObject.Find("FerrejeMenu");
+		BanMenu = GameObject.Find ("BanMenu");
+		KanslorMenu = GameObject.Find ("KanslorMenu");
 
         PrincipalMenu.SetActive(true);
         OptionsMenu.SetActive(false);
+		ExtrasMenu.SetActive(false);
+		RealmsMenu.SetActive(false);
+		StoriesMenu.SetActive(false);
+		FamiliaMenu.SetActive (false);
+		EscuelaMenu.SetActive (false);
+		AutoevaluacionMenu.SetActive (false);
+		FerrejeMenu.SetActive (false);
+		BanMenu.SetActive (false);
+		KanslorMenu.SetActive (false);
     }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+		PrincipalMenu.SetActive(false);
+		RealmsMenu.SetActive (true);
     }
-
-	public void Extras()
-	{
-		SceneManager.LoadScene ("ExtrasMenu");
-	}
 
     public void ExitGame()
     {
@@ -50,6 +72,114 @@ public class MainMenu : MonoBehaviour
 
         Debug.Log("Backing to PrincipalMenu...");
     }
+
+	public void Extras()
+	{
+		//SceneManager.LoadScene ("ExtrasMenu");
+		PrincipalMenu.SetActive(false);
+		ExtrasMenu.SetActive(true);
+		Debug.Log("Extras Button Clicked!");
+	}
+
+	public void Stories(){
+		ExtrasMenu.SetActive(false);
+		StoriesMenu.SetActive(true);
+	}
+
+	public void Familia(){
+		StoriesMenu.SetActive (false);
+		FamiliaMenu.SetActive (true);
+	}
+
+	public void Escuela(){
+		StoriesMenu.SetActive (false);
+		EscuelaMenu.SetActive (true);
+	}
+
+	public void AutoEva(){
+		StoriesMenu.SetActive (false);
+		AutoevaluacionMenu.SetActive (true);
+	}
+
+	public void Ferreje(){
+		RealmsMenu.SetActive (false);
+		FerrejeMenu.SetActive (true);
+	}
+
+	public void Ban(){
+		RealmsMenu.SetActive (false);
+		BanMenu.SetActive (true);
+	}
+
+	public void Kanslor(){
+		RealmsMenu.SetActive (false);
+		KanslorMenu.SetActive (true);
+	}
+
+	public void MainGoBack(string option)
+	{
+		PrincipalMenu.SetActive (true);
+
+		switch (option) 
+		{
+			case "Extras":
+				ExtrasMenu.SetActive (false);
+				break;
+			case "Options":
+				OptionsMenu.SetActive (false);
+				break;
+			case "Realms":
+				RealmsMenu.SetActive (false);
+				break;
+		}	
+	}
+
+	public void ExtrasGoBack(string option)
+	{
+		ExtrasMenu.SetActive (true);
+
+		switch(option)
+		{
+			case "Stories":
+				StoriesMenu.SetActive (false);
+				break;
+		}
+	}
+
+	public void StoriesGoBack(string option)
+	{
+		StoriesMenu.SetActive (true);
+
+		switch (option) {
+			case "Familia": 
+				FamiliaMenu.SetActive (false);
+				break;
+			case "Escuela":
+				EscuelaMenu.SetActive (false);
+				break;
+			case "Autoevaluacion":
+				AutoevaluacionMenu.SetActive (false);
+				break;
+		}
+	}
+
+	public void RealmsGoBack(string option)
+	{
+		RealmsMenu.SetActive (true);
+
+		switch (option) {
+		case "Ferreje": 
+			FerrejeMenu.SetActive (false);
+			break;
+		case "Ban":
+			BanMenu.SetActive (false);
+			break;
+		case "Kanslor":
+			KanslorMenu.SetActive (false);
+			break;
+		}
+	}
+
 
 
 }
