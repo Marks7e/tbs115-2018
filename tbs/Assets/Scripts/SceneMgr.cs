18169d62edd936220836514c7bd5603b4434053c
lusart <lusart87@gmail.com>
2018-10-09 22:00:01 -0600

    Agregado video de introduccion a la historia social 1

---
+++ tbs/Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Carga las Animaciones
+	public void loadAnimation(string pAnimacion){
+		SceneManager.LoadScene (pAnimacion);
+	}
+}
---
+++ tbs/Assets/Scripts/change.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change : MonoBehaviour {
+	public float reloj = 0;
+	public string sceneToChange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			reloj -= Time.deltaTime;
+			if(reloj<=0){
+				
+				//Application.LoadLevel(sceneToChange);
+				//UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToChange);
+				SceneManager.LoadScene(sceneToChange);
+			}
+	}
+
+}
