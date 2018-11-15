5b8d726393eb90578e08269b1f36475de28ee7e8
Fabiola Duarte <josselynfabiola@gmail.com>
2018-11-15 00:03:21 -0600

    Primer avance minijuego4

---
+++ tbs/Assets/Scripts/Historias/Historia4/IconDragger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IconDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public bool clonable;
+	public static Transform draggedIcon;
+	public Transform hand;
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		if(transform.childCount == 0)
+		{
+			eventData.pointerDrag = null;
+			return;
+		}
+
+		if(!this.clonable)
+		{
+			draggedIcon = transform.GetChild(0);
+			draggedIcon.SetParent(this.hand, false);
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		this.hand.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		if(draggedIcon == null) return;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ tbs/Assets/Scripts/Historias/Historia4/IconGetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IconGetter : MonoBehaviour, IDropHandler {
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		Transform droppedIcon;
+		droppedIcon = IconDragger.draggedIcon;
+
+		if(this.gameObject.CompareTag(droppedIcon.tag))
+		{
+			IconDragger.draggedIcon = null;
+			droppedIcon.SetParent(transform, false);
+		}
+		else
+		{
+			print("Error");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
