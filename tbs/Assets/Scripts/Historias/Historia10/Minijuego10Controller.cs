0a91a1ccf530f353b0d51b4fc8e1ce8401bf4cef
lusart <lusart87@gmail.com>
2019-03-14 01:20:01 -0600

    Agregado funcionamiento de rondas de juegos

--- tbs/Assets/Scripts/Historias/Historia10/Minijuego10Controller.cs
+++ tbs/Assets/Scripts/Historias/Historia10/Minijuego10Controller.cs
@@ -1,36 +1,30 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.DataPersistence.DependecyInjector;
+using Assets.Scripts.DataPersistence.Models;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Minijuego10Controller : MonoBehaviour
 {
-    
     public Button btnEmoji1,btnEmoji2,btnEmoji3,btnEmoji4,btnCompare;
 	private Image imgEmo1,imgEmo2,imgEmo3,imgEmo4; //Imagen sobre boton
-	public GameObject panel;
+	public GameObject panelBotones,panelSecuencia,btnReset, btnContinue;
 	public GameObject[] emoji; 
+    public GameObject msj_ok, msj_fail;
 	private int indice = 0, j = 0, k = 0, p = 0, q = 0;
     public Sprite[] spriteList;
+	public Sprite defaultBoton;
     private float elapsedTime = 0;
-	private float limitTime = 5;
+	private float hideTime = 5;
 	private bool updateOn = true;
-    
+	public Text Nivel;
+    private int count = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        btnEmoji1.onClick.AddListener(() => actions(1));
-		btnEmoji2.onClick.AddListener(() => actions(2));
-		btnEmoji3.onClick.AddListener(() => actions(3));
-		btnEmoji4.onClick.AddListener(() => actions(4));
-		btnCompare.onClick.AddListener(compareEmojis);
-
-		imgEmo1 = GameObject.Find("imgEmo1").GetComponent<Image>();
-		imgEmo2 = GameObject.Find("imgEmo2").GetComponent<Image>();
-		imgEmo3 = GameObject.Find("imgEmo3").GetComponent<Image>();
-		imgEmo4 = GameObject.Find("imgEmo4").GetComponent<Image>();
-
-		panel.SetActive(false);
+		GetAndInitializeAllGameObjects();
 
 		/* Funcion que crea secuencia de emojis de manera aleatoria */
 		randomSequence();
@@ -43,9 +37,9 @@ public class Minijuego10Controller : MonoBehaviour
 		if (updateOn == true)
 		{
 			elapsedTime += Time.deltaTime;
-			//Debug.Log("Tiempo: "+elapsedTime);
-			if(elapsedTime >= limitTime){
-				//Debug.Log("Se Acabo el tiempo, desaparece secuencia");
+			Debug.Log("Tiempo transcurrido: "+elapsedTime);
+			if(elapsedTime >= hideTime){
+				//Al alcanzar hideTime, oculta la secuencia de emojis
 				elapsedTime = 0;
 				updateOn = false;
 				hideSequence();
@@ -53,7 +47,6 @@ public class Minijuego10Controller : MonoBehaviour
 		}
 
     }
-
     public void randomSequence()
     {
         /* Llena el vector de emojis que se van a mostrar como secuencia de emojis */
@@ -62,6 +55,7 @@ public class Minijuego10Controller : MonoBehaviour
             /* Obtiene indice al azar para llamar a la imagen guardada en spriteList[] */
             indice = Random.Range(0, 5);
             emoji[i].GetComponent<Image>().sprite = spriteList[indice];
+			Debug.Log("posicion: "+i+" ,nombre: "+spriteList[indice].name);
         }
     }
     public void actions(int btn)
@@ -90,14 +84,15 @@ public class Minijuego10Controller : MonoBehaviour
 	}
     public void hideSequence()
 	{
-		panel.SetActive(true);
-
 		for (int i = 0; i < 4; i++)
 		{
+			//Oculta cada elemento de imagen de la secuencia
 			emoji[i].GetComponent<Image>().enabled = false;
 		}
 
-		panel.GetComponent<Animation>().Play("Panel2");
+		//Activa panel y animacion con botones
+		panelBotones.SetActive(true);
+		panelBotones.GetComponent<Animation>().Play("Panel2");
 
 	}
     public void setEmoji(Image imgEmo, int index)
@@ -107,22 +102,90 @@ public class Minijuego10Controller : MonoBehaviour
 	public void compareEmojis()
 	{
 		/* Comprobacion de secuencia con imagen del panel */
-		if (imgEmo1.sprite.name == emoji[0].GetComponent<Image>().sprite.name)
-		{	Debug.Log("Primer par son iguales");	}
-		else{	Debug.Log("Fallo, Termina el Juego");	}
-
-		if (imgEmo2.sprite.name == emoji[1].GetComponent<Image>().sprite.name)
-		{	Debug.Log("Segundo par son iguales");	}
-		else{	Debug.Log("Fallo, Termina el Juego");	}
-
-		if (imgEmo3.sprite.name == emoji[2].GetComponent<Image>().sprite.name)
-		{	Debug.Log("Tercer par son iguales");	}
-		else{	Debug.Log("Fallo, Termina el Juego");	}
-
-		if (imgEmo4.sprite.name == emoji[3].GetComponent<Image>().sprite.name)
-		{	Debug.Log("Cuarto par son iguales, GANASTE!!!!");	}
-		else{	Debug.Log("Fallo, Termina el Juego");	}
+		if (imgEmo1.sprite.name == emoji[0].GetComponent<Image>().sprite.name && 
+		imgEmo2.sprite.name == emoji[1].GetComponent<Image>().sprite.name && 
+		imgEmo3.sprite.name == emoji[2].GetComponent<Image>().sprite.name && 
+		imgEmo4.sprite.name == emoji[3].GetComponent<Image>().sprite.name)
+		{
+			Debug.Log("Todos Coinciden!!!");
+			Ok();
+		}else
+		{
+			Debug.Log("FALLO, Uno o mas no coinciden");
+			fail();
+		}
 		
 	}
+	
+	//Mensaje de respuesta correcta
+    public void Ok()
+    {
+        //UpdateScore();
+        //SettingTimeOfGame();
+        msj_ok.SetActive(true);
+        btnContinue.SetActive(true);
+		btnCompare.enabled = false;
+        //isRoundDone = true;
+    }
+    //Mensaje de Respuesta incorrecta
+    public void fail()
+    {
+        //score -= 800;
+        msj_fail.SetActive(true);
+        btnReset.SetActive(true);
+        //isRoundDone = true;
+    }
+	public void iteracion()
+    {
+        //isRoundDone = false;
 
+        //Setando Texto
+        Nivel.text = count + "/3";
+
+        //Contador
+        count++;
+
+        msj_ok.SetActive(false);
+        btnContinue.SetActive(false);
+		btnCompare.enabled = true;
+
+		//Coloca imagen base en boton
+		imgEmo1.sprite = defaultBoton;
+		imgEmo2.sprite = defaultBoton;
+		imgEmo3.sprite = defaultBoton;
+		imgEmo4.sprite = defaultBoton;
+
+		panelBotones.SetActive(false);
+		
+        //Generar una nueva secuencia
+		
+		if(count <= 3){
+			randomSequence();
+			updateOn = true;
+			for (int i = 0; i < 4; i++)
+			{
+				//Visualizar cada elemento de imagen de la secuencia
+				emoji[i].GetComponent<Image>().enabled = true;
+			}
+		}else{
+			Debug.Log("----**********--- JUEGO FINALIZADO ----**********---");
+		}
+
+    }
+	private void GetAndInitializeAllGameObjects()
+    {
+		btnEmoji1.onClick.AddListener(() => actions(1));
+		btnEmoji2.onClick.AddListener(() => actions(2));
+		btnEmoji3.onClick.AddListener(() => actions(3));
+		btnEmoji4.onClick.AddListener(() => actions(4));
+		btnCompare.onClick.AddListener(compareEmojis);
+
+		imgEmo1 = GameObject.Find("imgEmo1").GetComponent<Image>();
+		imgEmo2 = GameObject.Find("imgEmo2").GetComponent<Image>();
+		imgEmo3 = GameObject.Find("imgEmo3").GetComponent<Image>();
+		imgEmo4 = GameObject.Find("imgEmo4").GetComponent<Image>();
+
+		panelBotones.SetActive(false);
+
+	}
 }
