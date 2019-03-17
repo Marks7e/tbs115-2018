9dda5db90b19533be0ea701ea889afc44311a586
Marcos Barrera <mmbarrerae@gmail.com>
2019-03-17 00:25:30 -0600

    Agregada funcion para evaluar si debe o no mostrar el boton de Skip: Configurado para jugar al menos 5 veces primero antes de habilitar el boton de skip

--- tbs/Assets/Scripts/VideoControl.cs
+++ tbs/Assets/Scripts/VideoControl.cs
@@ -3,49 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class VideoControl : MonoBehaviour {
+public class VideoControl : MonoBehaviour
+{
+    public EnableSkip enableSkip;
+    public GameObject skipButton;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    public GameObject btnPlay, btnPause;
+    public string sceneToChange;
+    public float duration;
+    private bool videostate;
 
-	public VideoPlayer videoPlayer;
-	public AudioSource audioSource;
-	public GameObject btnPlay,btnPause;
-	public string sceneToChange;
-	public float duration;
-	private bool videostate;
+    // Use this for initialization
+    void Start()
+    {
+        enableSkip = new EnableSkip();
+        skipButton = GameObject.Find("Saltar");
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(videoPlayer.isPlaying){
-			videostate = true;
-			btnPlay.SetActive(false);
-			btnPause.SetActive(true);
-			Debug.Log("Esta reproduciendo"+", "+"frame: "+videoPlayer.frame+", "+"tiempo: "+videoPlayer.time);
-		}else{
-			videostate = false;
-			btnPause.SetActive(false);
-			btnPlay.SetActive(true);
-			Debug.Log("Esta Pausado"+" "+"frame: "+videoPlayer.frame+", "+"tiempo: "+videoPlayer.time);
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videostate = true;
+            btnPlay.SetActive(false);
+            btnPause.SetActive(true);
+            enableSkip.EnableSkipButton(getLevelNumberFromSceneToLoad(sceneToChange), 5, skipButton);
+            //Debug.Log("Esta reproduciendo"+", "+"frame: "+videoPlayer.frame+", "+"tiempo: "+videoPlayer.time);
+        }
+        else
+        {
+            videostate = false;
+            btnPause.SetActive(false);
+            btnPlay.SetActive(true);
+            enableSkip.EnableSkipButton(getLevelNumberFromSceneToLoad(sceneToChange), 5, skipButton);
+            //Debug.Log("Esta Pausado"+" "+"frame: "+videoPlayer.frame+", "+"tiempo: "+videoPlayer.time);
+        }
 
-		if(videoPlayer.time >= duration){
-			Debug.Log("cambio de escena");
-			SceneManager.LoadScene(sceneToChange);
-		}
+        if (videoPlayer.time >= duration)
+        {
+            //Debug.Log("cambio de escena");
+            SceneManager.LoadScene(sceneToChange);
+        }
 
-	}
+    }
 
-	public void actionPause(){		
-		videoPlayer.Pause();
-		audioSource.Pause();
-	}
+    public void actionPause()
+    {
+        videoPlayer.Pause();
+        audioSource.Pause();
+    }
 
-	public  void actionPlay(){
-		videoPlayer.Play();
-		audioSource.Play();
-	}
+    public void actionPlay()
+    {
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+
+    private int getLevelNumberFromSceneToLoad(string sceneToChange)
+    {
+        return int.Parse(sceneToChange.Split(' ')[1]);
+    }
 }
