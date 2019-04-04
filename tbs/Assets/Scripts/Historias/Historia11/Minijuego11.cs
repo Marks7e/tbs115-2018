6ae673887b3aca62b087ac333b8cbd7a47590f0a
lusart <lusart87@gmail.com>
2019-04-04 03:16:49 -0600

    Correccion de audio, falta mensajes y persistencia

--- tbs/Assets/Scripts/Historias/Historia11/Minijuego11.cs
+++ tbs/Assets/Scripts/Historias/Historia11/Minijuego11.cs
@@ -17,21 +17,34 @@ public class Minijuego11 : MonoBehaviour, IHasChanged
 
     public Button btnCompare;
 
+    /*
     public AudioSource audioAlegria;
     public AudioSource audioTristeza;
     public AudioSource audioEnojo;
     public AudioSource audioMiedo;
+    */
+
+    public AudioSource audioPetition;
+    public AudioClip[] audioClipArray; 
 
     private int nchar;
     private int option;
-    private int count = 0;
+    private int count = 1;
     private int i = 0;
 
-    private bool stateSlots = false;
+    private void Awake()
+    {
+        audioPetition = GetComponent<AudioSource>();
 
+    }
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        audioPetition.clip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+        audioPetition.PlayOneShot(audioPetition.clip);
+        */
+
         GetAndInitializeAllGameObjects();
 
         HasChanged();
@@ -43,120 +56,153 @@ public class Minijuego11 : MonoBehaviour, IHasChanged
     // Update is called once per frame
     void Update()
     {
-        
         Debug.Log("UPDATE valor de elementText: ------------- " + elementText.text);
         Debug.Log("UPDATE valor de referenciaOPcion: ------------- "+referenciaOpcion);
-
-        if (elementText.text.Length > 10)
-        {
-            panelSprites.gameObject.SetActive(false); //Desactivar panel de sprites
-            
-            stateSlots = true; //
-            //Debug.Log("Estan Seteados los 2 slots? :" + stateSlots);
-            btnCompare.gameObject.SetActive(true); //Activar el boton de comparar
-        }
+        Debug.Log(" palabra tiene menos de 10 letras");
+        /*
+        if (count <= 3)
+        {*/
+            if (elementText.text.Length > 10)
+            {
+                Debug.Log(" palabra tiene mas de 10 letras");
+                DisablePanelSprites(); //Funcion que desactiva panel de sprites y activa boton
+            }
+       /* }
         else
         {
-            Debug.Log("Estan Seteados los 2 slots? :" + stateSlots);
-        }
+            Debug.Log("--*-*-*-*-*-*-*-*-*-*- Juego Terminado -*-*-*-*-*-*-**-*-*-*-*-*--*");
+        }*/
         
-  
+         
     }
 
     public void GetAndInitializeAllGameObjects()
     {
-        randomPetition();
+        RandomPetition();
+    }
 
-        audioAlegria = GetComponent<AudioSource>();
-        audioTristeza = GetComponent<AudioSource>();
-        audioEnojo = GetComponent<AudioSource>();
-        audioMiedo = GetComponent<AudioSource>();
+    public void DisablePanelSprites()
+    {
+        panelSprites.gameObject.SetActive(false);
+        btnCompare.gameObject.SetActive(true);
+    }
+
+    public void EnablePanelSprite()
+    {
+        btnCompare.gameObject.SetActive(false);  //Desactiva boton
+        panelSprites.gameObject.SetActive(true); //Activa panel
+    }
+
+    public void resetStage()
+    {
+        elementText.text = " ";
+
+        referenciaOpcion = " ";
+
+        EnablePanelSprite();
+        
+        //Borra los sprite cargado en rostro de emoji
+        foreach (Transform slotTransform in slots)
+        {
+            GameObject itemSprite = slotTransform.GetComponent<SlotContent>().item;
+            Destroy(itemSprite);
+        }
+
+        //Borra todos los sprite arrastrables
+        foreach (Transform spriteTransform in sprites)
+        {
+            GameObject objSprite = spriteTransform.GetComponent<SlotContent>().item;
+            Destroy(objSprite);
+        }
+
+        //Vuelve a llenar todo el panel de sprites arrastrables
+        foreach (Transform spriteTransform in sprites)
+        {
+            GameObject objectHijo = Instantiate(arrayPrefab[i]) as GameObject;
+            objectHijo.name = arrayPrefab[i].name;
+            objectHijo.transform.parent = spriteTransform.transform;
+            objectHijo.transform.position = spriteTransform.transform.position;
+            i++;
+        }
+
+        i = 0;
+        
     }
 
     public void Validate()
     {
+        /*
         Debug.Log("VALIDATE Valor de Referencia: " + referenciaOpcion);
         Debug.Log("VALIDATE Valor de ElementText: " + elementText.text);
-        
+      */
+        Debug.Log("intento: " + count);
 
-        if (elementText.text == referenciaOpcion)
+        if (count <= 3)
         {
-            Debug.Log("Son iguales, ACERTASTE");
-            count++;
-            elementText.text = " "; //Para que no siga entrando al if de update
-            referenciaOpcion = " ";
-            btnCompare.gameObject.SetActive(false);  //Desactiva boton
-            panelSprites.gameObject.SetActive(true); //Activa panel
-            Debug.Log("count: "+count);
-
-            //Borra los sprite cargado en rostro de emoji
-            foreach (Transform slotTransform in slots)
-            { 
-                GameObject itemSprite = slotTransform.GetComponent<SlotContent>().item;
-                Destroy(itemSprite);
-            }
-
-            //Borra todos los sprite arrastrables
-            foreach (Transform spriteTransform in sprites)
+            if (elementText.text == referenciaOpcion)
             {
-                GameObject objSprite = spriteTransform.GetComponent<SlotContent>().item;
-                Destroy(objSprite);
-            }
+                Debug.Log("Son iguales, ACERTASTE");
 
-            //Vuelve a llenar todo el panel de sprites arrastrables
-            foreach (Transform spriteTransform in sprites)
-            {
-                GameObject objectHijo = Instantiate(arrayPrefab[i]) as GameObject;
-                objectHijo.name = arrayPrefab[i].name;
-                objectHijo.transform.parent = spriteTransform.transform;
-                objectHijo.transform.position = spriteTransform.transform.position;
-                i++;
+               
+                count++;
+
+                resetStage();
+                RandomPetition();
             }
-            i = 0; //reiniciar indice de arrayprefab
-            
-           // randomPetition(); //Randomizar nuevamente
-           // HasChanged();
-           // Update();
-            
-            
+            else
+            {
+                Debug.Log("Son diferentes, FALLASTE");
+                count = 0; //reinicia los intentos
+            }
         }
         else
         {
-            Debug.Log("Son diferentes, FALLASTE");
-            
+            Debug.Log("--*-*-*-*-*-*-*-*-*-*- Juego Terminado -*-*-*-*-*-*-**-*-*-*-*-*--*");
         }
 
     }
 
-    public void randomPetition()
+    public void RandomPetition()
     {
-        
-        switch (Random.Range(0, 5))
+        int op = UnityEngine.Random.Range(0, 5);
+
+        switch (op)
         {
             case 0:
                 Debug.Log("Reproducir mp3 Alegria");
-                audioAlegria.Play();
+                //audioAlegria.Play();
+                audioPetition.clip = audioClipArray[0];
+                //audioPetition.PlayOneShot(audioPetition.clip);
                 referenciaOpcion = "o_alegreb_alegre";
                 break;
             case 1:
                 Debug.Log("Reproducir mp3 Triste");
-                audioTristeza.Play();
+                //audioTristeza.Play();
+                audioPetition.clip = audioClipArray[1];
+                //audioPetition.PlayOneShot(audioPetition.clip);
                 referenciaOpcion = "o_tristezab_tristeza";
                 break;
             case 2:
                 Debug.Log("Reproducir mp3 Miedo");
-                audioMiedo.Play();
+                //audioMiedo.Play();
+                audioPetition.clip = audioClipArray[2];
+                //audioPetition.PlayOneShot(audioPetition.clip);
                 referenciaOpcion = "o_miedob_miedo";
                 break;
             case 3:
                 Debug.Log("Reproducir mp3 Enojo");
-                audioEnojo.Play();
+                //audioEnojo.Play();
+                audioPetition.clip = audioClipArray[3];
+                //audioPetition.PlayOneShot(audioPetition.clip);
                 referenciaOpcion = "o_enojob_enojo";
                 break;
         }
 
+        audioPetition.PlayOneShot(audioPetition.clip);
+
+
     }
-    
+
     public void HasChanged()
     {
         System.Text.StringBuilder builder = new System.Text.StringBuilder();
