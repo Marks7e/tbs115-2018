adcdb96c502c73c8825f572ad8b3d79f70346ef0
Marcos Barrera <mmbarrera@gmail.com>
2019-04-09 17:50:07 -0600

    Agregado grafico de dashboard y botnes de estadistica por reino. Aun pendiente por terminar

---
+++ tbs/Assets/Scripts/Historias/PostGameTest/TestResultDashboard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestResultDashboard : MonoBehaviour
+{
+    private GameObject _chartObject;
+    private Image _charImage;
+    private float _waitTime = 1.5f;
+
+    void Start()
+    {
+        InitializeAllObjects();
+    }
+
+    void Update()
+    {
+        AnimateGraph();
+    }
+
+    private void InitializeAllObjects()
+    {
+        _charImage = GameObject.Find("chartObject").GetComponent<Image>();
+    }
+
+
+    private void AnimateGraph()
+    {
+        _charImage.fillAmount += 1.0f / _waitTime * Time.deltaTime;
+    }
+}
