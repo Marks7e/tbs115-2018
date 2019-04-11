cfeea74a3e9facf19d339959e34b2d1af83b3073
Marcos Barrera <mmbarrera@gmail.com>
2019-04-10 19:35:50 -0600

    Pendiente realizar funciones para mostrar datos y consideraciones

--- tbs/Assets/Scripts/Historias/PostGameTest/TestResultDashboard.cs
+++ tbs/Assets/Scripts/Historias/PostGameTest/TestResultDashboard.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.DataPersistence.DependecyInjector;
+using Assets.Scripts.DataPersistence.Models;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +10,10 @@ public class TestResultDashboard : MonoBehaviour
     private GameObject _chartObject;
     private Image _charImage;
     private float _waitTime = 1.5f;
+    private List<QuestionData> _questionDataListModelForRealm1;
+    private List<QuestionData> _questionDataListModelForRealm2;
+    private List<QuestionData> _questionDataListModelForRealm3;
+    private DependencyInjector _di;
 
     void Start()
     {
@@ -15,14 +21,39 @@ public class TestResultDashboard : MonoBehaviour
     }
     void Update()
     {
-        AnimateGraph();
+        AnimateGraph(_waitTime);
     }
     private void InitializeAllObjects()
     {
+        _di = new DependencyInjector();
+        _questionDataListModelForRealm1 = new List<QuestionData>();
+        _questionDataListModelForRealm2 = new List<QuestionData>();
+        _questionDataListModelForRealm3 = new List<QuestionData>();
         _charImage = GameObject.Find("chartObject").GetComponent<Image>();
     }
-    private void AnimateGraph()
+    private void InitializeDataForGraph()
     {
-        _charImage.fillAmount += 1.0f / _waitTime * Time.deltaTime;
+        _questionDataListModelForRealm1 = _di.GetAllRealmQuestionForRealm(1);
+        _questionDataListModelForRealm2 = _di.GetAllRealmQuestionForRealm(2);
+        _questionDataListModelForRealm3 = _di.GetAllRealmQuestionForRealm(3);
+    }
+    private void AnimateGraph(int realm, float animationWaitTime)
+    {
+
+        _charImage.fillAmount += 1.0f / animationWaitTime * Time.deltaTime;
+    }
+    private float GetFillForGraph(List<QuestionData> listQuestionData)
+    {
+        int yesCount = 0;
+        float fillAmount = 0f;
+
+        foreach (QuestionData questionData in listQuestionData)
+        {
+            if (questionData.Answer == "S")
+                yesCount += 1;
+        }
+
+        fillAmount = ((360 / listQuestionData.Count) * yesCount);
+        return fillAmount;
     }
 }
