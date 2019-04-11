5823b0d300a926a4644b142839014a881aefc263
lusart <lusart87@gmail.com>
2019-04-11 00:15:19 -0600

    Agregado Minijuego 11, en rama creada a partir de development_DefensaPublica

---
+++ tbs/Assets/Scripts/Historias/Historia11/DragHandeler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandeler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemBeingDragger;
+    Vector3 startPosition;
+    Transform startParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragger = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragger = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+        }
+       
+    }
+}
---
+++ tbs/Assets/Scripts/Historias/Historia11/SlotContent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotContent : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            DragHandeler.itemBeingDragger.transform.SetParent(transform);
+            ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
+        }
+    }
+}
