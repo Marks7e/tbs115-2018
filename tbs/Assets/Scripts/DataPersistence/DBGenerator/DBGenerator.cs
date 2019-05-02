f0e23693aa8c52122e1c377f3cc0daa7d709624c
Marcos Barrera <mmbarrerae@gmail.com>
2019-05-01 19:53:58 -0600

    Agregado sql para minijuego 9 en DBGenerator

--- tbs/Assets/Scripts/DataPersistence/DBGenerator/DBGenerator.cs
+++ tbs/Assets/Scripts/DataPersistence/DBGenerator/DBGenerator.cs
@@ -39,8 +39,9 @@ namespace Assets.Scripts.DataPersistence.DBGenerator
             _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (1,1,0,10,2,0,0)");
             _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (2,1,0,10,2,0,0)");
             _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (3,1,0,10,2,0,0)");
-            _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (4,1,0,10,2,0,0)");
-            _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (6,1,0,10,2,0,0)");
+            _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (4,2,0,10,2,0,0)");
+            _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (6,2,0,10,2,0,0)");
+            _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (9,3,0,10,2,0,0)");
             _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (10,3,0,10,2,0,0)");
             _cretaDatabaseListModel.Add("INSERT INTO 'LevelData' ('levelId','Realm','BestScore','RoundTime','PointMultiplier','UnlockLevelAt','TimesPlayed') VALUES (11,3,0,10,2,0,0)");
         }
