c444cb3312222769593e897509e0cf7910577cad
Fabiola Duarte <josselynfabiola@gmail.com>
2019-05-04 00:31:37 -0600

    Creando escenario para minijuego 5

---
+++ tbs/Assets/Scripts/Historias/Historia5/IconDraggerGame5.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IconDraggerGame5 : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public bool clonable;
+	public static Transform draggedIcon;
+	public Transform hand;
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		if(transform.childCount == 0)
+		{
+			eventData.pointerDrag = null;
+			return;
+		}
+
+		if(!this.clonable)
+		{
+		
+			if(this.gameObject.CompareTag("Arbol"))
+			{
+				draggedIcon = transform.GetChild(0);
+				DestroyImmediate(transform.gameObject);
+			}
+			else
+			{
+				draggedIcon = transform.GetChild(0);
+				draggedIcon.SetParent(this.hand, false);
+			}
+			
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		this.hand.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		if(draggedIcon == null) return;
+
+		draggedIcon.SetParent(transform, false);
+		draggedIcon = null;
+	}
+}
---
+++ tbs/Assets/Scripts/Historias/Historia5/IconGetterGame5.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IconGetterGame5 : MonoBehaviour, IDropHandler
+{
+    /*public GameStatus gameStatusModel;
+    public AudioSource audioSource;
+    public int waitingTime = 3;
+    public DependencyInjector dependecyInjector;
+
+    void Start()
+    {
+        dependecyInjector = new DependencyInjector();
+        GetInitializeMusic();
+    }*/
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Transform droppedIcon;
+        droppedIcon = IconDragger.draggedIcon;
+
+        if (this.gameObject.CompareTag(droppedIcon.tag))
+        {
+            if (transform.childCount > 0)
+            {
+                IconDragger.draggedIcon = transform.GetChild(0);
+            }
+            else
+            {
+                IconDragger.draggedIcon = null;
+            }
+            droppedIcon.SetParent(transform, false);
+            //Bar.slots[GetComponent<Slot>().id] = 1;
+        }
+        else
+        {
+            if (transform.childCount > 0)
+            {
+                IconDragger.draggedIcon = transform.GetChild(0);
+            }
+            else
+            {
+                IconDragger.draggedIcon = null;
+            }
+            //Bar.slots[GetComponent<Slot>().id] = 0;
+            droppedIcon.SetParent(transform, false);
+            //LoseGame();
+        }
+    }
+
+    /*void GetInitializeMusic()
+    {
+        audioSource = GameObject.Find("Barra").GetComponent<AudioSource>();
+    }
+
+    void LoseGame()
+    {
+        dependecyInjector.UpdateLevelTimesPlayed(4);
+        audioSource.Stop();
+        gameStatusModel = new GameStatus();
+        gameStatusModel.PlayerNeedToRepeatGame(audioSource, waitingTime, 4);
+    }*/
+}
