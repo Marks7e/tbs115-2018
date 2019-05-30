014770f40f00ac662692237c7b2805c751eba7c1
Fabiola Duarte <josselynfabiola@gmail.com>
2019-05-30 12:30:21 -0600

    Segundo avance minijuego 5

--- tbs/Assets/Scripts/Historias/Historia5/Minijuego5.cs
+++ tbs/Assets/Scripts/Historias/Historia5/Minijuego5.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minijuego5 : MonoBehaviour
 {
+
+    public Button[] emojiArrow;
+    public Sprite arrow;
+    public int _iArrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomPositionArrow();
     }
 
     // Update is called once per frame
@@ -15,4 +21,11 @@ public class Minijuego5 : MonoBehaviour
     {
         
     }
+
+    private void RandomPositionArrow(){
+        _iArrow = Random.Range(0, 4);
+        emojiArrow[_iArrow].GetComponent<Image>().sprite = arrow;
+        Debug.Log("posicion: "+ _iArrow);
+    }
+
 }
