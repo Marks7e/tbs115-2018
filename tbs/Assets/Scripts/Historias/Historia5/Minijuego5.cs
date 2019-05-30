185dbe700ee130ff7ada212e86b9e575c04d91c6
Fabiola Duarte <josselynfabiola@gmail.com>
2019-05-30 13:27:30 -0600

    Verificando slot id con item, minijuego 5

--- tbs/Assets/Scripts/Historias/Historia5/Minijuego5.cs
+++ tbs/Assets/Scripts/Historias/Historia5/Minijuego5.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Minijuego5 : MonoBehaviour
+public class Minijuego5 : MonoBehaviour, IHasChanged
 {
+    [SerializeField] Transform slots;
+    [SerializeField] Text elementText;
 
     public Button[] emojiArrow;
     public Sprite arrow;
-    public int _iArrow;
+    public int _iArrow, _iSlot;
 
     // Start is called before the first frame update
     void Start()
     {
         RandomPositionArrow();
+        HasChanged();
+        CheckBall();
     }
 
     // Update is called once per frame
@@ -28,4 +33,32 @@ public class Minijuego5 : MonoBehaviour
         Debug.Log("posicion: "+ _iArrow);
     }
 
+    //Obtiene el valor del sprite posicionado en el rostro de emojis
+    public void HasChanged()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+   
+        foreach (Transform slotTransform in slots)
+        {
+            GameObject item = slotTransform.GetComponent<SlotContent>().item;
+            if (item)
+            {
+                builder.Append(item.name);
+                _iSlot = slotTransform.GetComponent<Slot>().id;
+                Debug.Log("slot: "+ _iSlot);
+            }
+        }
+        elementText.text = builder.ToString();
+    }
+
+    private void CheckBall(){
+        if(_iArrow == _iSlot){
+            Debug.Log("slot: "+ _iSlot + " posicion: "+ _iArrow);
+        }
+        else{
+            Debug.Log("No slot: "+ _iSlot + " posicion: "+ _iArrow);
+        }
+    }
 }
+
+
